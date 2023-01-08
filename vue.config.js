bdf69e47d7c60330909b9a4803932f1accd58b15
kimddddh98 <amawang11@gmail.com>
2023-01-08 23:32:32 +0900

    pages path

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ?'githubPageTest/'
-    :'/',
-    outputDir: 'docs'
+  publicPath: '/Youtube-Api',
+  outputDir: 'docs'
 })
